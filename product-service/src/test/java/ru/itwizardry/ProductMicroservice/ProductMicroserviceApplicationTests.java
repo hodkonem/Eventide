14f149a961c637809a62bffcedda83277ea4f86e
Mikhail Latypov <mikhaillat@yandex.ru>
2025-05-03 15:11:11 +0300

    add product-service

---
+++ product-service/src/test/java/ru/itwizardry/ProductMicroservice/ProductMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.itwizardry.ProductMicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
